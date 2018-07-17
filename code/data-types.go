c37615a46559fc1abe242f365828ff9f3f3b0c07
Ross Larson <zo0o0ot@gmail.com>
2018-07-16 22:29:42 -0500

    updates data types with more comments

--- code/data-types.go
+++ code/data-types.go
@@ -6,16 +6,16 @@ import (
 ) // These are called packages.  They help you do cool stuff.
 
 var (
-	ToBe        bool       = false
-	z           complex128 = cmplx.Sqrt(-4) // Fancy types exist.
+	ToBe        bool       = false          // Basic types: int, string, bool
+	z           complex128 = cmplx.Sqrt(-4) // Others: byte, rune, uint32, float64, uintptr
 	notAssigned int                         // If not assigned, value defaults to zero.
 ) // You can initialize variables outside of a function
 
 func main() {
-	var ThatString string = "That" // Full variable initialization
-	d6 := 6                        // Shorter syntax, also types can be inferred
-	RosesRed, VioletsBlue, GolangSweet, YouAreToo := true, true, true, true
+	var ThatRhyme string = "A Poem:" // Full variable initialization
+	RosesRed := true                 // Shorter syntax, also types can be inferred
+	ThatVariable, GolangSweet, YouAreToo := 2, true, true
 	fmt.Println(ToBe, z, notAssigned)
-	fmt.Println(ThatString, d6)
-	fmt.Println(RosesRed, VioletsBlue, GolangSweet, YouAreToo)
+	fmt.Println(ThatRhyme)
+	fmt.Println(RosesRed, ThatVariable, GolangSweet, YouAreToo)
 }
