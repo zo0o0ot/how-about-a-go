666c22ca26265727ce2fc977d2e8165b3ddd0c59
Ross Larson <zo0o0ot@gmail.com>
2018-07-20 23:20:58 -0500

    Adds some more map stuff.  Maybe len next?

--- code/maps/maps.go
+++ code/maps/maps.go
@@ -6,7 +6,22 @@ import (
 
 func showSomeMaps() {
 	itsamap := make(map[string]int)
+	itsamap["a"] = 1
+	itsamap["b"] = 52
+	itsamap["c"] = 4
+	itsamap["d"] = 20
+
 	fmt.Println(itsamap)
+	delete(itsamap, "d")         //
+	_, isPresent := itsamap["d"] // Note the two return values.
+	// Also "_" refers to a variable that we are going to ignore.
+
+	strangemap := make(map[rune]string)
+
+	fmt.Println("Roll for initiative with d20?", isPresent)
+	fmt.Println(itsamap)
+	fmt.Println(strangemap)
+
 }
 
 func main() {
