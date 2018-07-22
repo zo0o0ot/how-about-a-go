724f4a95bba947f24d41fb3e897b353b26318e26
Ross Larson <zo0o0ot@gmail.com>
2018-07-22 16:17:38 -0500

    updates compilation error slide

--- code/compilation-error/compilation-error.go
+++ code/compilation-error/compilation-error.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	notUsed := "at all" // Variable is declared but not used. Golang doesn't like that.
-	fmt.Println("This won't display because of a compilation error.")
+	fmt.Println("Will this display?")
 }
