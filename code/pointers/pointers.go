c81f50b4325a923ff2d5d24a20d78cb83c032260
Ross Larson <zo0o0ot@gmail.com>
2018-07-25 15:54:44 -0500

    remove debug file, update pointer notes

--- code/pointers/pointers.go
+++ code/pointers/pointers.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	powerLevel := 9000
 	fmt.Println(powerLevel)
-	jumpStreet := &powerLevel                        // point powerLevel to jumpStreet
+	jumpStreet := &powerLevel                        // point jumpStreet to powerLevel
 	fmt.Println(*jumpStreet)                         // read powerLevel through the pointer
 	*jumpStreet = 21                                 // set powerLevel through the pointer
 	fmt.Println(powerLevel, *jumpStreet, jumpStreet) // see the new value of variables and pointer
