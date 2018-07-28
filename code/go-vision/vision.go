6e466c96a4a88a2b4061bd35bcb4ed3d399dc6f3
Ross Larson <zo0o0ot@gmail.com>
2018-07-28 14:21:12 -0500

    adds vision API images and code

---
+++ code/go-vision/vision.go
@@ -0,0 +1,45 @@
+// Sample vision-quickstart uses the Google Cloud Vision API to label an image.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	// Imports the Google Cloud Vision API client package.
+	vision "cloud.google.com/go/vision/apiv1"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the name of the image file to annotate.
+	filename := "testdata/clark.png"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	defer file.Close()
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+
+	labels, err := client.DetectLabels(ctx, image, nil, 10)
+	if err != nil {
+		log.Fatalf("Failed to detect labels: %v", err)
+	}
+
+	fmt.Println("Labels:")
+	for _, label := range labels {
+		fmt.Println(label.Description)
+	}
+}
