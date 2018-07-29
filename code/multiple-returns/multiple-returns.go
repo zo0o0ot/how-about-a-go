1a97aa42b4f229ad716c3a062ef307427c91c7c6
Ross Larson <zo0o0ot@gmail.com>
2018-07-28 21:20:47 -0500

    updates return values code for readability

--- code/multiple-returns/multiple-returns.go
+++ code/multiple-returns/multiple-returns.go
@@ -6,12 +6,12 @@ func randomishNumber() int {
 	return 7 // Well, it may have been random at one point. https://xkcd.com/221/
 }
 
-func instructions() (string, string) {
-	return "When I say That,", "You say Conference"
-}
-
 func callAndResponse() string {
 	return "That"
+}
+
+func instructions() (string, string) {
+	return "When I say That,", "You say Conference"
 }
 
 func main() {
